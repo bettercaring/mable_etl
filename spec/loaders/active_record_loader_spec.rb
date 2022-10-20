4cdf7984cb3ffe5d687edd48a4b660c2a75f405e
Stacey Brosnan <staceybrosnan@gmail.com>
2022-10-20 16:07:05 +1100

    ES-14815 - added data cleaner for specs

--- spec/loaders/active_record_loader_spec.rb
+++ spec/loaders/active_record_loader_spec.rb
@@ -6,11 +6,11 @@ require 'loaders/active_record_loader'
 RSpec.describe MableEtl::Loaders::ActiveRecordLoader do
   subject(:active_record_loader) { described_class.new(data) }
   let(:table_name) { User }
-  let!(:number) { table_name.count + 2 }
+  # let!(:number) { table_name.count + 2 }
   let(:data) do
-    [{ id: table_name.last.id + 1, name: 'name', email: 'chicken@gmail.com' },
-     { id: table_name.last.id + 1, name: 'gerald', email: 'chicken@gmail.com' },
-     { id: table_name.last.id + 2, name: 'hello', email: 'hello@gmail.com' }]
+    [{ id: 1, name: 'name', email: 'chicken@gmail.com' },
+     { id: 1, name: 'gerald', email: 'chicken@gmail.com' },
+     { id: 2, name: 'hello', email: 'hello@gmail.com' }]
   end
 
   before do
@@ -22,7 +22,7 @@ RSpec.describe MableEtl::Loaders::ActiveRecordLoader do
   describe '#load' do
     it 'adds the data to the table' do
       active_record_loader.load
-      expect(User.count).to eq(number)
+      expect(User.count).to eq(2)
     end
   end
 end
