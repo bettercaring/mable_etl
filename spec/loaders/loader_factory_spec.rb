3147f6731b65c590f94ccdc3c41b52681b5953bd
Stacey Brosnan <staceybrosnan@gmail.com>
2022-10-25 09:59:10 +1100

    ES-14815 - spec tidy

--- spec/loaders/loader_factory_spec.rb
+++ spec/loaders/loader_factory_spec.rb
@@ -1,19 +1,26 @@
-# require 'spec_helper'
-# require 'loaders/loader_factory'
+require 'spec_helper'
+require 'loaders/loader_factory'
 
-# RSpec.describe MableEtl::Loaders::LoaderFactory do
-#   subject(:loader_factory) { described_class.for(data, config_loader, loader_type) }
-#   let(:config_loader) { User }
-#   let(:loader_type) { 'ActiveRecord' }
-#   let(:data) do
-#     [{ id: 1, name: 'name', email: 'chicken@gmail.com' },
-#      { id: 1, name: 'gerald', email: 'chicken@gmail.com' },
-#      { id: 2, name: 'hello', email: 'hello@gmail.com' }]
-#   end
+RSpec.describe MableEtl::Loaders::LoaderFactory do
+  subject(:loader_factory) { described_class.for(data, config_loader, loader_type) }
+  let(:config_loader) { User }
+  let(:loader_type) { 'ActiveRecordLoader' }
+  let(:data) do
+    [{ id: 1, name: 'name', email: 'chicken@gmail.com' },
+     { id: 1, name: 'gerald', email: 'chicken@gmail.com' },
+     { id: 2, name: 'hello', email: 'hello@gmail.com' }]
+  end
+  let(:active_record_loader) { instance_double(MableEtl::Loaders::ActiveRecordLoader) }
+  let(:load_result) { true }
 
-#   describe '#for' do
-#     it 'adds the data to the table' do
-#        loader_factory
-#     end
-#   end
-# end
+  before do
+    allow(MableEtl::Loaders::ActiveRecordLoader).to receive(:new).and_return(active_record_loader)
+    allow(active_record_loader).to receive(:load).and_return(load_result)
+  end
+
+  describe 'loader factory' do
+    it 'sends data to the correct loader' do
+      expect(loader_factory).to eq(load_result)
+    end
+  end
+end
