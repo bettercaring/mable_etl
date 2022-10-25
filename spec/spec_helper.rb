1b54234aaa78ece2a72be1149ee43c8c9f99660c
Stacey Brosnan <staceybrosnan@gmail.com>
2022-10-26 10:09:23 +1100

    ES-14815 - refactor

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require "mable_etl"
-require 'support/database_cleaner'
+require 'database_cleaner/active_record'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
